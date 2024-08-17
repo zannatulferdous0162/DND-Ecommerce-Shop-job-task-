f09404aceb506edd18e3ecdef193fddda994d71a
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-17 18:23:43 +0600

    change  middleBanner

--- src/Components/MidelBanner/MidelBanner.jsx
+++ src/Components/MidelBanner/MidelBanner.jsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import './Midel.css'; // Keep if you have additional styles
-// import banner from '../../assets/banner-03.jpg';
+import React from 'react';
+import './Midel.css'; // Keep if you have additional styles
+import banner from '../../assets/banner-03.jpg';
 
-// const MidelBanner = () => {
-//     return (
-//         <div className="mt-10 relative overflow-hidden">
-//             <div
-//                 className="h-[400px] bg-fixed bg-cover bg-center flex flex-col items-start justify-center"
-//                 style={{ backgroundImage: `url(${banner})` }}
-//             >
-//                 <div className="p-8 ">
-//                     <p className="text-xl text-white font-semibold">Limited Time Offer</p>
-//                     <h1 className="text-4xl text-white font-bold my-4">
-//                         Limited Time Offer Special Edition
-//                     </h1>
-//                     <p className="text-lg text-white mb-6">Buy This T-shirt At 20% Discount, Use Code OFF20</p>
-//                     <button className="btn">
-//                         SHOP NOW
-//                     </button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const MidelBanner = () => {
+    return (
+        <div className="mt-10 relative overflow-hidden">
+            <div
+                className="h-[400px] bg-fixed bg-cover bg-center flex flex-col items-start justify-center"
+                style={{ backgroundImage: `url(${banner})` }}
+            >
+                <div className="p-8 ">
+                    <p className="text-xl text-white font-semibold">Limited Time Offer</p>
+                    <h1 className="text-4xl text-white font-bold my-4">
+                        Limited Time Offer Special Edition
+                    </h1>
+                    <p className="text-lg text-white mb-6">Buy This T-shirt At 20% Discount, Use Code OFF20</p>
+                    <button className="btn">
+                        SHOP NOW
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 
-// export default MidelBanner;
+export default MidelBanner;
